eb88ca8dd39122bf6dd39d5810baf8c1c0264c62
andewalle <awtudor@gmail.com>
2019-05-14 13:02:04 +0200

    css fixes

--- src/components/GameLeader.vue
+++ src/components/GameLeader.vue
@@ -2,6 +2,7 @@
   <div class="question-box-container">
     <b-jumbotron>
       <template slot="lead">
+        <b-img src="~/static/simon.jpg"></b-img>
         <p>Fråga</p>
       </template>
       <hr class="my-4">
@@ -18,8 +19,16 @@
           </b-col>
         </b-row>
       </b-list-group>
-        <b-row class="imageRow"> 
+        <b-row class="imageRow">
+          <div class="imgDiv">
         <b-img src="http://www.freespins24h.se/wp-content/uploads/2017/02/faq.png" fluid alt="Responsive image"></b-img>
+        </div>
+        <div class="imgDiv">
+        <b-img src="http://www.freespins24h.se/wp-content/uploads/2017/02/faq.png" fluid alt="Responsive image"></b-img>
+        </div>
+        <div class="imgDiv">
+        <b-img src="http://www.freespins24h.se/wp-content/uploads/2017/02/faq.png" fluid alt="Responsive image"></b-img>
+        </div>
         </b-row>
       <b-button class="submit">Submit</b-button>
       <b-col class="rulesMenu">
@@ -60,14 +69,55 @@ a {
   color: #42b983;
 }
 
+.imgDiv{
+  padding: 3rem;
+}
+
+.jumbotron{
+  background: #ee0979;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #ff6a00, #ee0979);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #ff6a00, #ee0979); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  
+}
+
+.submit{
+  background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+
+.submit:hover{
+  cursor: pointer;
+    box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+    transform: translateY(-0.1rem);
+    transition: transform 150ms;
+}
+
+.rules{
+   background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+
+.rules:hover{
+  cursor: pointer;
+    box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+    transform: translateY(-0.1rem);
+    transition: transform 150ms;
+}
+
 .formss{
   margin: 2rem;
-  margin-left: 12rem;
+  margin-left: 20rem;
   
 }
 
 .img{
   width: 200px;
+  display: flex;
+  
 }
 
 .rules{
@@ -76,6 +126,11 @@ a {
 }
 
 .imageRow{
-  margin-left: 10rem;
+  margin-left: 17rem;
+  display: flex;
 }
+
+
+
+
 </style>
