8d6bbd067209493c7ca92c8f5cad39c607f4a19b
Helena <helena@bulle.nu>
2019-05-16 22:14:00 +0200

    added a gameleader, textbox for questions etc

--- src/components/GameLeader.vue
+++ src/components/GameLeader.vue
@@ -1,17 +1,34 @@
 <template>
   <b-container fluid class="text-center">
     <b-row align-v="center">
-      
-      
     </b-row>
     <b-row>
     <b-col align-self="end">
       <b-button class="rules">Rules</b-button>
     </b-col>
     </b-row>
+    <b-row class="justify-content-md-center"> 
+      <b-col col lg=2>
+        <b-img src="https://cdn.pixabay.com/photo/2017/12/21/10/30/mascot-3031512_960_720.png" height="400px"></b-img>
+      </b-col>
+      <b-col col lg=2>
+        <b-row>
+        <b-card style="max-width: 20rem;">
+          <b-card-text width="100">
+            Fråga ställs i denna rutan! Frågor på det?
+          </b-card-text>
+        </b-card>
+        </b-row>
+        <br />
+        <b-row>
+                <b-alert variant="success" show>Success Alert</b-alert>
+                <b-alert variant="danger" show>För högt/För lågt</b-alert>
+        </b-row>
+      </b-col>
+    </b-row>
+    <b-row>&nbsp;</b-row>
   </b-container>
 </template>
-
   <script>
 export default {
   name: "GameLeader",
@@ -80,88 +97,88 @@ export default {
 </script>
 
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
+<style scoped>
 h3 {
-  margin: 40px 0 0;
+ margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+ list-style-type: none;
+ padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+ display: inline-block;
+ margin: 0 10px;
 }
 a {
-  color: #42b983;
+ color: #42b983;
 }
 
 .imgDiv{
-  padding: 3rem;
+ padding: 3rem;
 }
 
 .jumbotron{
-  background: #ee0979;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #ff6a00, #ee0979);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #ff6a00, #ee0979); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  background-size: cover;
-  height: 100vh;
-  width: 100vh;
-  
+ background: #ee0979;  /* fallback for old browsers */
+ background: -webkit-linear-gradient(to right, #ff6a00, #ee0979);  /* Chrome 10-25, Safari 5.1-6 */
+ background: linear-gradient(to right, #ff6a00, #ee0979); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+ background-size: cover;
+ height: 100vh;
+ width: 100vh;
+
 }
 
 .submit{
-  background: #000000;  /* fallback for old browsers */
+ background: #000000;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 }
 
 .submit:hover{
-  cursor: pointer;
-    box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
-    transform: translateY(-0.1rem);
-    transition: transform 150ms;
+ cursor: pointer;
+   box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+   transform: translateY(-0.1rem);
+   transition: transform 150ms;
 }
 
 .rules{
-   background: #000000;  /* fallback for old browsers */
+  background: #000000;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 }
 
 .rules:hover{
-  cursor: pointer;
-    box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
-    transform: translateY(-0.1rem);
-    transition: transform 150ms;
+ cursor: pointer;
+   box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+   transform: translateY(-0.1rem);
+   transition: transform 150ms;
 }
 
 .formss{
-  margin: 2rem;
-  margin-left: 20rem;
-  
+ margin: 2rem;
+ margin-left: 20rem;
+
 }
 
 .img{
-  width: 200px;
-  display: flex;
-  
+ width: 200px;
+ display: flex;
+
 
 }
 
 .rules{
-  
-  float:right;
+
+ float:right;
 }
 
 .imageRow{
-  margin-left: 17rem;
-  display: flex;
+ margin-left: 17rem;
+ display: flex;
 }
 
-
-
-
+.boxed {
+ border: 1px solid green ;
+}
 </style>
