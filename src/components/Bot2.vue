9e956bf3c9362d9bc87f8b28518d771be46bb940
robin hansson <robin-hansson@hotmail.com>
2019-05-17 18:14:35 +0200

    if/else for setting answerMin and answerMax in bots

--- src/components/Bot2.vue
+++ src/components/Bot2.vue
@@ -1,42 +1,52 @@
 <template>
+    <templateFix v-bind:player="this.$store.getters.getBot2" v-bind:guess="guess"/>
 </template>
 
 <script>
-import { EventBus } from './event-bus.js';
+import { EventBus } from '../event-bus.js';
+import templateFix from './templateFix.vue';
 
 export default {
     name: "Bot2",
-    points: 0,
+    points: 0,    
+    components:{
+        templateFix
+    },
     props: {
-    msg: String
+        msg: String
     },
     data() {
         return {
-        text: ''
+        guess: ''
         }
     },
     methods: {
         autoGuess() {            
 
-            let min = this.$store.getters.getAnswerMin;  //Get min and max values from question 
-            let max = this.$store.getters.getAnswerMax;
+            let min;
+            let max;
 
-            let guess = randomNr(min, max);  
+            if(getAnswerMin == null || getAnswerMax == null) {
+                min = this.$store.getters.getAnswerMin;  //Get min and max values from question 
+                max = this.$store.getters.getAnswerMax;
+            }
+            else {
+                min = 1;
+                max = 1000000;
+            }   
 
-            //Vad händer här? hur hanteras gissningen?     return eller skicka?
-            //Hur plussa poäng?
-            EventBus.$emit('bot-guessings', guess);
-            
+            let guess = randomNr(min, max)
+
+            setTimeout(() => {
+                this.guess = guess
+                EventBus.$emit('answerSent', guess);
+            }, 3000);        
+
         },
         randomNr(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
     },
-    mutations: {
-        addPoint() {     //Använda detta?
-            points++;
-        }
-    }
 };
 </script>
 
