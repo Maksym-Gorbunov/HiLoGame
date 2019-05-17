2a695595c852c2cb9d2df6327b0d54b749f43f49
Simon Seabird <m13simon@gmail.com>
2019-05-17 10:28:21 +0100

    Img fixed

--- src/store.js
+++ src/store.js
@@ -14,24 +14,24 @@ export default new Vuex.Store({
             type: "user",
             name: "",
             score: 0,
-            imgURL: "",
+            imgURL: "https://banner2.kisspng.com/20180715/zq/kisspng-red-royalty-free-clip-art-confused-cartoon-5b4b22a2d9a6d5.1339546915316507228915.jpg",
             active: false
         },
         bot1: {
             type: "bot",
             name: "bot1",
             score: 0,
-            imgURL: "",
+            imgURL: "https://library.kissclipart.com/20180902/ebw/kissclipart-cartoon-robots-clipart-robot-clip-art-8d5c25496d8a30ad.jpg",
             active: false
         },
         bot2: {
             type: "bot",
             name: "bot2",
             score: 0,
-            imgURL: "",
+            imgURL: "https://library.kissclipart.com/20180915/xxe/kissclipart-einstein-salesforce-clipart-salesforce-com-service-d8b8f4e7390da218.png",
             active: false
         },
-        players: [], 
+        players: [],
         questions: [],
         currentPlayer: null,
         currentQuestion: null
@@ -71,13 +71,21 @@ export default new Vuex.Store({
             console.log('question:' + data[0].difficulty)
         },
         nextTurn(state){
-            let indexOfCurrentPlayer = this.players.indexOf(state.currentPlayer);
+            let indexOfCurrentPlayer = this.players.findIndex(i => i.active);
 
             if (indexOfCurrentPlayer < 2) {
                 state.currentPlayer = state.players[indexOfCurrentPlayer++];
             }
             else{
                 state.currentPlayer = state.players[0];
+            }
+        },
+        nextQuestion(){
+            if(currentQuestion == null){
+                currentQuestion = questions[0];
+            }
+            else{
+                let indexOfCurrentPlayer = this.questions.findIndex(i => i.que);
             }
         }
     },
