f8d0027eb4157e4d064218ebc18e96229f56a708
andewalle <awtudor@gmail.com>
2019-05-20 14:32:50 +0200

    dunno

--- src/components/playerData.vue
+++ src/components/playerData.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <b-img class="img-responsive" v-if="player.imgURL" v-bind:src="getImgUrl(player.imgURL)"></b-img>
-    <b-img
-      class="img-responsive"
-      v-else-if="player.active"
-      src="http://www.freespins24h.se/wp-content/uploads/2017/02/faq.png">
-    </b-img>
     <div>{{player.name}}</div>
     <div>Score: {{player.score}}</div>
   </div>
