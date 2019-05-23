417a1095b0aa4981d73bb0c5a580c3f549be74fd
Helena <helena@bulle.nu>
2019-05-23 23:25:38 +0200

    created speech boxes, fixed size on bot boxes

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,16 +1,15 @@
 <template>
-
-<b-col sm="1" md="3" lg="4" align-self="center">
-    <b-input-group v-if="activePlayer"
-		 v-bind:class="{active: activePlayer}">
-        <b-form-input v-model="userInput" type="number" v-bind:disabled="answerSent"></b-form-input>
-            <b-input-group-append>
-                <b-button variant="info" v-on:click="sendAnswer()" v-bind:disabled="!validUserInput || answerSent">Submit</b-button>
-            </b-input-group-append>
-    </b-input-group>
-		 <playerData v-bind:player="this.$store.getters.getUser"/>
-  </b-col>
- 
+	<b-col sm="1" md="3" lg="4" align-self="center">
+		<div class="speech-bubble" v-if="activePlayer" v-bind:class="{active: activePlayer}">
+			<b-input-group>
+			<b-form-input v-model="userInput" type="number" v-bind:disabled="answerSent"></b-form-input>
+			<b-input-group-append>
+			<b-button variant="info" v-on:click="sendAnswer()" v-bind:disabled="!validUserInput || answerSent">Submit</b-button>
+			</b-input-group-append>
+			</b-input-group>
+		</div>
+		<playerData v-bind:player="this.$store.getters.getUser"/>
+	</b-col>
 </template>
 
 <script>
@@ -55,5 +54,26 @@
 <style scoped>
 .img_responsive {
   margin-top: 10rem;
+}
+
+.speech-bubble {
+	position: relative;
+	background: #ffffff;
+	border-radius: .4em;
+}
+
+.speech-bubble:after {
+	content: '';
+	position: absolute;
+	bottom: 0;
+	left: 50%;
+	width: 0;
+	height: 0;
+	border: 16px solid transparent;
+	border-top-color: #ffffff;
+	border-bottom: 0;
+	border-right: 0;
+	margin-left: -8px;
+	margin-bottom: -16px;
 }
 </style>
