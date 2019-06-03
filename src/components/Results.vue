c833a6ca65346e8e99d673c335b9db9de31b569c
andewalle <awtudor@gmail.com>
2019-06-03 12:25:45 +0200

    endscreen fix

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <b-modal id="modal-center" ref="my-modal" hide-footer title="Using Component Methods">
+    <b-modal id="modal-center" ref="my-modal" hide-footer title="Game Finished!">
       <div class="d-block text-center">
-        <h3>Congrats {{$store.getters.getCurrentPlayer.name}} you won!</h3>
+        <h3 v-if="$store.getters.getCurrentPlayer.type == 'user'">Congrats {{$store.getters.getCurrentPlayer.name}} you won!</h3>
+        <h3 v-else>Better luck next time!</h3>
       </div>
       <b-button class="mt-3" variant="outline-danger" block @click="restartGame()">Play Again</b-button>
     </b-modal>
