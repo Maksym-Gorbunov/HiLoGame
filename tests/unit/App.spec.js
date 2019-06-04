89a3f6015cc04b046a8fc7d6da1a8615e008869e
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-04 07:56:16 +0200

    test for verifying App.vue is an vue instance

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -5,10 +5,13 @@ var should = require('chai').should;
 var assert = require('assert');
 
 describe('App.vue', ()=>{
-
+    
     const wrapper = shallowMount(App);
+    it('App.vue should be an instance of vue', ()=>{
 
+        expect(wrapper.isVueInstance());
+    });
     it('name should be equal to \'app\'', ()=>{
         assert.equal(wrapper.name(), 'app');
-    })
-})
+    });
+});
