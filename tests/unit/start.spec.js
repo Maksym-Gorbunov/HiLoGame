60c01110a19b40259f227ff937b8970f2de97371
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-04 08:06:03 +0200

    mounting + basic tests

--- tests/unit/start.spec.js
+++ tests/unit/start.spec.js
@@ -5,3 +5,18 @@ const expect = require('chai').expect;
 const should = require('chai').should();
 const assert = require('assert');
 const sinon = require('sinon');
+
+describe('Start.vue', ()=>{
+    
+    
+    const wrapper = shallowMount(Start);
+    it('Start.vue should be an instance of vue', ()=>{
+        expect(wrapper.isVueInstance())
+    })
+    describe('Start', ()=>{
+        it('name should equal \'start\'', ()=>{
+            const name = wrapper.name();
+            name.should.equal('start');
+
+    })
+})
