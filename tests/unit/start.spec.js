17ecd8554c82de4ca5a8cb9c7adc28f9542c0192
Gustav Ljungberg <g.ljungberg@live.se>
2019-06-04 07:59:12 +0200

    added imports

---
+++ tests/unit/start.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+import Start from '@/components/Start.vue'
+
+const expect = require('chai').expect;
+const should = require('chai').should();
+const assert = require('assert');
+const sinon = require('sinon');
